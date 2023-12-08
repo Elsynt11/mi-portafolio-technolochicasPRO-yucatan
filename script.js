a596dfee9699c8a45afbe62d6e8c173734c45695
Elsy Noh <144864149+Elsynt11@users.noreply.github.com>
2023-12-08 01:17:03 -0600

    Testimonios

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-const nombre = 'Elsy Karina Noh Tuyub';
-let rol = 'Desarrolladora Web Jr';
+const nombre = "Elsy Karina Noh Tuyub";
+let rol = "Desarrolladora Web Jr";
 
 //alert(nombre);
 //console.log(nombre);
 //document.write(nombre);
 
-let elememtoHTMLTitulo = document.getElementById('titulo');
+let elememtoHTMLTitulo = document.getElementById("titulo");
 
 let elementoDeTypewriter = new Typewriter(elememtoHTMLTitulo, {
-    loop: true
+  loop: true,
 });
 
-elementoDeTypewriter.typeString(nombre)
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString(rol)
-    .pauseFor(2500)
-    .start()
+elementoDeTypewriter
+  .typeString(nombre)
+  .pauseFor(2500)
+  .deleteAll()
+  .typeString(rol)
+  .pauseFor(2500)
+  .start();
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    center: true,
+    items: 3,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+    },
+  });
+});
